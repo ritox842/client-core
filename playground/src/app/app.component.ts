3693e00b0a80492d342a8cafc71b3bfe5e60f592
Amit Bh <amit@datorama.com>
2018-03-27 14:27:55 +0300

    fix(css): fix site responsiveness

--- playground/src/app/app.component.ts
+++ playground/src/app/app.component.ts
@@ -1,17 +1,20 @@
-import {Component} from '@angular/core';
+import { Component, ViewEncapsulation } from "@angular/core";
 
 @Component({
-  selector: 'app',
-  styleUrls: [ './app.component.scss' ],
-  templateUrl: './app.component.html'
+  selector: "app",
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: ["./app.component.scss"],
+  templateUrl: "./app.component.html"
 })
 export class AppComponent {
   changeTheme() {
-    document.body.classList.toggle('dark');
-    window[ 'theme' ] = document.body.classList.contains('dark') ? 'dark' : 'light';
+    document.body.classList.toggle("dark");
+    window["theme"] = document.body.classList.contains("dark")
+      ? "dark"
+      : "light";
 
-    Array.from(document.querySelectorAll('.panel-block')).forEach(e => {
-      e.classList.toggle('dark');
+    Array.from(document.querySelectorAll(".panel-block")).forEach(e => {
+      e.classList.toggle("dark");
     });
   }
 }

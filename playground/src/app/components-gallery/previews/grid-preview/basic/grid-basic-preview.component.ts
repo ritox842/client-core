bf8004df5adcf0cf649897ca441e07e686a60853
Amit Bh <amit@datorama.com>
2018-03-28 19:40:39 +0300

    fix(component): fix filter list

--- playground/src/app/components-gallery/previews/grid-preview/basic/grid-basic-preview.component.ts
+++ playground/src/app/components-gallery/previews/grid-preview/basic/grid-basic-preview.component.ts
@@ -1,12 +1,12 @@
-import {Component} from '@angular/core';
-import {timer} from "rxjs/observable/timer";
-import {concatMap, mapTo, tap} from "rxjs/operators";
-import {DatoGrid} from "../../../../../../../lib/src/grid/dato-grid";
-import {GridColumns, ToolbarAction} from "../../../../../../../lib";
+import { Component } from "@angular/core";
+import { timer } from "rxjs/observable/timer";
+import { concatMap, mapTo, tap } from "rxjs/operators";
+import { DatoGrid } from "../../../../../../../lib/src/grid/dato-grid";
+import { GridColumns, ToolbarAction } from "../../../../../../../lib";
 
 @Component({
-  selector: 'dato-grid-basic-preview',
-  templateUrl: './grid-basic-preview.component.html'
+  selector: "dato-grid-basic-preview",
+  templateUrl: "./grid-basic-preview.component.html"
 })
 export class GridBasicPreviewComponent extends DatoGrid<any> {
   data;
@@ -14,28 +14,38 @@ export class GridBasicPreviewComponent extends DatoGrid<any> {
   ngOnInit() {
     super.ngOnInit();
 
-    this.asyncRows().pipe(
-      tap(( data ) => this.data = data),
-      concatMap(( grid ) => this.datoGridReady)
-    ).subscribe(( rows ) => {
-      this.setRows(this.data);
-    });
+    this.asyncRows()
+      .pipe(
+        tap(data => (this.data = data)),
+        concatMap(grid => this.datoGridReady)
+      )
+      .subscribe(rows => {
+        this.setRows(this.data);
+      });
   }
 
   getColumns(): GridColumns {
     return [
       {
-        headerName: 'ID',
-        field: 'id',
+        headerName: "ID",
+        field: "id",
         width: 100,
         filter: "agNumberColumnFilter"
       },
       {
-        headerName: 'Value',
-        field: 'value',
+        headerName: "Value",
+        field: "value",
         width: 100,
         filter: "agTextColumnFilter"
       },
+      {
+        valueGetter(params) {
+          const { id, value } = params.data;
+
+          return `${id} ${value}`;
+        },
+        headerName: "IdValue"
+      }
     ];
   }
 
@@ -45,15 +55,13 @@ export class GridBasicPreviewComponent extends DatoGrid<any> {
 
   asyncRows() {
     let rows = [];
-    for ( let i = 0; i < 1000; i ++ ) {
-      rows.push({id: i, value: i * 5});
+    for (let i = 0; i < 1000; i++) {
+      rows.push({ id: i, value: i * 5 });
     }
-    return timer(3000).pipe(mapTo(rows))
-
+    return timer(3000).pipe(mapTo(rows));
   }
 
-  myCustomLogic( selectedRows: any[] ) {
-    return selectedRows.some(( row ) => row.value === 5);
+  myCustomLogic(selectedRows: any[]) {
+    return selectedRows.some(row => row.value === 5);
   }
-
 }

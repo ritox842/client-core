e00ae8e77a96d9e9d47c0d4e833e44880b3fe9af
Amit Bh <amit@datorama.com>
2018-05-02 18:39:52 +0300

    fix(component): icon - add width and height atts plus ie11 fix

--- playground/src/main.browser.ts
+++ playground/src/main.browser.ts
@@ -1,14 +1,16 @@
 /**
  * Angular bootstrapping
  */
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { environment } from 'environments/environment';
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { environment } from "environments/environment";
+
+require("core-js");
 
 /**
  * App Module
  * our top level module that holds all of our components
  */
-import { AppModule } from './app';
+import { AppModule } from "./app";
 
 /**
  * Bootstrap our Angular app with a top level NgModule
@@ -17,7 +19,7 @@ export function main(): Promise<any> {
   return platformBrowserDynamic()
     .bootstrapModule(AppModule)
     .then(environment.decorateModuleRef)
-    .catch((err) => console.error(err));
+    .catch(err => console.error(err));
 }
 
 /**
@@ -25,16 +27,16 @@ export function main(): Promise<any> {
  * in prod this is replace for document ready
  */
 switch (document.readyState) {
-  case 'loading':
-    document.addEventListener('DOMContentLoaded', _domReadyHandler, false);
+  case "loading":
+    document.addEventListener("DOMContentLoaded", _domReadyHandler, false);
     break;
-  case 'interactive':
-  case 'complete':
+  case "interactive":
+  case "complete":
   default:
     main();
 }
 
 function _domReadyHandler() {
- document.removeEventListener('DOMContentLoaded', _domReadyHandler, false);
- main();
+  document.removeEventListener("DOMContentLoaded", _domReadyHandler, false);
+  main();
 }

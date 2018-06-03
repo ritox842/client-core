19dfd0cc34cb4b7f882c169d415c6a7ef60842e0
Amit Bh <amit@datorama.com>
2018-06-03 19:08:40 +0300

    chore(package): bump version

--- lib/src/grid/public_api.ts
+++ lib/src/grid/public_api.ts
@@ -8,10 +8,15 @@
 
 export * from './grid/grid.component';
 export * from './grid-pagination/grid-pagination.component';
+
 export * from './grid-toolbar/grid-toolbar.component';
 export * from './grid-toolbar/grid-toolbar-item.directive';
 export * from './grid-toolbar/grid-toolbar.types';
+export * from './grid-toolbar/grid-toolbar.service';
+export * from './grid-toolbar/grid-toolbar-handlers';
+
 export * from './grid-header/grid-header.component';
 export * from './grid.module';
 export * from './dato-grid';
+export * from './dato-grid-api';
 export * from './grid-config';

0f03956fefb08c99ea33dcbc197be12ae3657abc
Inbal Sinai <inbal.sinai@datorama.com>
2018-07-31 12:11:50 +0300

    fix(datocarousel): added api export

--- lib/src/public_api.ts
+++ lib/src/public_api.ts
@@ -24,6 +24,7 @@ export * from './snackbar/public_api';
 export * from './editable-header/public_api';
 export * from './select/public_api';
 export * from './list/public_api';
+export * from './carousel/public_api';
 export * from './options/public_api';
 export * from './tabs/public_api';
 export * from './dialog/public_api';
